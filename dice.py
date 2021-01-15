7ace23f281a875f7215580975e7088cef1e6f7fc
ckyiu <ckyiu@outlook.com>
2021-01-14 21:33:15 -0500

    Parameters for roll and ping

--- dice.py
+++ dice.py
@@ -1,10 +1,21 @@
 import random
 
 
-def roll(sides: int = 6):
-    return random.randint(1, sides + 1)
+def roll(sides: int = 6, times: int = 1) -> list:
+    dice_rolls = []
+    for _ in range(times):
+        dice_rolls.append(random.randint(1, sides + 1))
+    return dice_rolls
 
 
-def roll_nice_text(sides: int = 6):
-    value = roll(sides=sides)
-    return f"> 🎲 You got: {value}! 🎲", value
+def roll_nice_text(sides: int = 6, times: int = 1) -> tuple:
+    text = "```\n🎲 You got: "
+    values = []
+    for index, value in enumerate(roll(sides=sides, times=times)):
+        if index == 0:
+            text += str(value)
+        else:
+            text += f", {value}"
+        values += str(value)
+    text += "! 🎲\n```"
+    return text, values
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
-import dice
+from dice import roll_nice_text
+from ping import ping_nice_text
 from create_logger import create_logger
 import logging
 
@@ -20,20 +21,21 @@ async def on_ready():
 
 
 @bot.command(name="ping")
-async def ping(ctx):
+async def ping(ctx, average_times: int = 1):
     logger.debug(f"Ping requested from {repr(ctx.guild)}")
-    ping = round(bot.latency * 1000)
-    logger.debug(f"Ping is {ping} ms!")
-    await ctx.send(f"> 🏓 Pong! 🏓 Ping is {ping} ms!")
-
-
-@bot.command(name="roll")
-async def roll(ctx):
-    logger.debug(f"Dice roll requested from {repr(ctx.guild)}")
-    sides = 6
-    text, value = dice.roll_nice_text(sides=sides)
-    logger.debug(f"Dice with {sides} sides rolled a {value}!")
+    text, _ = ping_nice_text(bot=bot, times=average_times)
+    logger.debug(f"Sent: {repr(text)}")
     await ctx.send(text)
 
 
+@bot.command(name="roll")
+async def roll(ctx, sides: int = 6, times: int = 1):
+    logger.debug(f"Dice roll requested from {repr(ctx.guild)}")
+    logger.debug(f"Parameters: sides = {repr(sides)}, times = {repr(times)}")
+    text, value = roll_nice_text(sides=sides, times=times)
+    logger.debug(f"Sent: {repr(text)}")
+    await ctx.send(text)
+
+
+logger.debug(f"Connecting to Discord...")
 bot.run(TOKEN)
